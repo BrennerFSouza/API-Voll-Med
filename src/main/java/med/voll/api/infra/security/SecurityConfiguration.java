cfe474310cfb1fa08aa80da8b12894db12b47674
brenner.souza <brenner.souza@gmail.com>
2024-12-17 12:13:08 -0300

    Testando o controle de acesso

--- src/main/java/med/voll/api/infra/security/SecurityConfiguration.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package med.voll.api.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,11 +12,15 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Autowired
+    private SecurityFilter securityFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(csrf -> csrf.disable())
@@ -24,6 +29,7 @@ public class SecurityConfiguration {
                             req.requestMatchers("/login").permitAll();
                             req.anyRequest().authenticated();
                         })
+                        .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                         .build();
     }
 
