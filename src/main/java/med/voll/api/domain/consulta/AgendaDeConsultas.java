0026ba6311adb4c981cb05ac1bdd8291a089080d
brenner.souza <brenner.souza@gmail.com>
2024-12-20 13:21:38 -0300

    Escolha de médico aleatório

--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -40,6 +40,14 @@ public class AgendaDeConsultas {
     }
 
     private Medico escolherMedico(DadosAgendamentoConsulta dados) {
-        return null;
+        if (dados.idMedico() != null){
+            return medicoRepository.getReferenceById(dados.idMedico());
+        }
+
+        if (dados.especialidade() == null){
+            throw new ValidacaoException("Especialidade é obrigatória quando médico não for escolhido");
+        }
+
+        return medicoRepository.escolherMedicoAleatorioLivreNaData(dados.especialidade(), dados.data());
     }
 }
